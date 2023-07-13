6ac78b0cbe20b4a6101fe4b3f7d3cdf5f04b3e3e
sakshijain20 <jain.sakshi1099@gmail.com>
2023-07-13 13:12:24 +0530

    added validation dependencies and validation for model class

--- src/main/java/com/springboot/blogapp/models/User.java
+++ src/main/java/com/springboot/blogapp/models/User.java
@@ -1,19 +1,31 @@
 package com.springboot.blogapp.models;
-
 import java.util.HashSet;
 import java.util.Set;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
 @Document(collection = "users")
 public class User {
   @Id
-  private String id; 
+  private String id;
+
+  @NotBlank
+  @Size(max = 20)
   private String username;
+
+  @NotBlank
+  @Size(max = 50)
+  @Email
   private String email;
+
+  @NotBlank
+  @Size(max = 120)
   private String password;
 
   @DBRef
