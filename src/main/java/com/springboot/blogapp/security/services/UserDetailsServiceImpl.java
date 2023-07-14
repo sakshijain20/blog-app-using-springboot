6983dd2534690dd87c078fe684bc2a04bd1570f9
sakshijain20 <jain.sakshi1099@gmail.com>
2023-07-15 00:51:55 +0530

    implemented security and added controllers

---
+++ src/main/java/com/springboot/blogapp/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.springboot.blogapp.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.springboot.blogapp.models.User;
+import com.springboot.blogapp.repository.UserRepositry;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	@Autowired
+	UserRepositry userRepository;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+		return UserDetailsImpl.build(user);
+	}
+
+}
