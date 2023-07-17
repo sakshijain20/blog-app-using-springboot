1151fba9e3998feb88d4e23e79fe5630d3ea9f29
sakshijain20 <jain.sakshi1099@gmail.com>
2023-07-17 23:30:43 +0530

    refactored aut contoller and removed mod role

--- src/main/java/com/springboot/blogapp/controllers/BlogPostController.java
+++ src/main/java/com/springboot/blogapp/controllers/BlogPostController.java
@@ -9,23 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
-public class TestController {
+public class BlogPostController {
+	
   @GetMapping("/all")
   public String allAccess() {
     return "Public Content.";
   }
   
   @GetMapping("/user")
-  @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+  @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
   public String userAccess() {
     return "User Content.";
   }
 
-  @GetMapping("/mod")
-  @PreAuthorize("hasRole('MODERATOR')")
-  public String moderatorAccess() {
-    return "Moderator Board.";
-  }
 
   @GetMapping("/admin")
   @PreAuthorize("hasRole('ADMIN')")
